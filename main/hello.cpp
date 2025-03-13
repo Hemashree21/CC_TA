6a7d96ac3afe095fbf5b2fd2456528613572b25a
Hack2Health <hemashree21.msh@gmail.com>
2025-03-13 21:26:04 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, PES1UG22CS238\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully made changes\n";
+  cout << "Webhook successfully enabled\n";
   return 0;
 }
