a2a5c88626ffce2d90c12e927053b1e80e0ec46c
Hack2Health <hemashree21.msh@gmail.com>
2025-03-13 20:51:22 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, PES1UG22CS238\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully made changes\n";
   return 0;
 }
